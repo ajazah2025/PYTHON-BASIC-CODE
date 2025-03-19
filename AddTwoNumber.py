3d1c0d7211e778f7b34f6deaada48a6d0005f534
ajazah2025 <ajazah2025@gmai.com>
2025-03-18 23:11:41 -0500

    First Commit

---
+++ AddTwoNumber.py
@@ -0,0 +1,11 @@
+#Adding two numbers by using function.
+def add (a,b):
+    return a+b
+
+#Initializing numbers
+a= int(input("Enter first number:"))
+b= int(input("Enter second number:"))
+
+#Calling  function
+result = add(a,b)
+print("Addtion of two number is",result)
