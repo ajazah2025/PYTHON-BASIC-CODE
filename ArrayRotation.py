320b7e2742e273914fcc47b98777a1ff5daca184
ajazah2025 <ajazah2025@gmai.com>
2025-03-21 17:15:03 -0500

    1st Commit

---
+++ ArrayRotation.py
@@ -0,0 +1,16 @@
+'''
+Input:  arr[] = [1, 2, 3, 4, 5, 6, 7]
+         d = 2
+Output: arr[] = [3, 4, 5, 6, 7, 1, 2] 
+
+''' 
+#Function to reverse Arr[]
+def rverseArray(arr,d):
+    c = arr[d:] + arr[:d] 
+    return c
+
+#Driver function to test the above function
+arr =[1,2,3,4,5,6,7,8,9,10]
+d = 2
+result = rverseArray(arr,d) #calling function
+print("The Array Rotation is",result)
