43f075db46e28fb2f3edd8a98aca1f8f733c1d9e
ajazah2025 <ajazah2025@gmai.com>
2025-03-21 14:54:47 -0500

    1st Commit

---
+++ SumofArray.py
@@ -0,0 +1,14 @@
+#Creating function to sum of array
+def Array_sum(arr):
+    sum = 0
+    for i in arr:
+        sum = sum + i
+    return sum
+
+#main function
+if __name__== "__main__":
+    arr = [1,2,3]                     #input value to the list
+    result= Array_sum(arr)            #calling function
+
+print("The sum of array is",result)   #printing value on screen 
+
