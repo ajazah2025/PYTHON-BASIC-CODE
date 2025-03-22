f4f48b1b0cafb980cc4f5d133420c2716740d768
ajazah2025 <ajazah2025@gmai.com>
2025-03-22 00:06:29 -0500

    1st Commit

---
+++ RemainderofArray.py
@@ -0,0 +1,18 @@
+#Python Program for Find remainder of array multiplication divided by n
+
+'''
+input: arr[] = {100, 10, 5, 25, 35, 14}, 
+            n = 11
+Output: 9
+
+'''
+def Remainder(arr):
+    multiple = 1
+    for i in arr:
+        multiple = multiple * i
+    return multiple % 11
+
+#Initializing the value and calling the function.    
+arr = [100, 10, 5, 25, 35, 14]
+result= Remainder(arr)
+print("Remainder of given Arrays:", result)
